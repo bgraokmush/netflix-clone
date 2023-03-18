35539d3d2dee1a7eafafd67b7f0dd9043cbfc2b4
Bugra Okumus <saltuk.bugraokumus@gmail.com>
2023-03-18 20:35:19 +0300

    bug fix

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,19 +1,42 @@
-import React, { useCallback, useState } from "react";
-import Input from "../components/input";
 import axios from "axios";
-import { signIn } from "next-auth/react";
+import { useCallback, useState } from "react";
+import { NextPageContext } from "next";
+import { getSession, signIn } from "next-auth/react";
 import { useRouter } from "next/router";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
-const auth = () => {
+import Input from "@/components/input";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
+const Auth = () => {
   const router = useRouter();
+
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
   const [variant, setVariant] = useState("login");
+
   const toggleVariant = useCallback(() => {
     setVariant((currentVariant) =>
-      currentVariant == "login" ? "register" : "login"
+      currentVariant === "login" ? "register" : "login"
     );
   }, []);
 
@@ -25,6 +48,7 @@ const auth = () => {
         redirect: false,
         callbackUrl: "/",
       });
+
       router.push("/");
     } catch (error) {
       console.log(error);
@@ -38,6 +62,7 @@ const auth = () => {
         name,
         password,
       });
+
       login();
     } catch (error) {
       console.log(error);
@@ -45,74 +70,78 @@ const auth = () => {
   }, [email, name, password, login]);
 
   return (
-    <div
-      className="relative h-full w-full bg-[url('/images/hero.jpg')]
-                     bg-no-repeat bg-center bg-fixed bg-cover"
-    >
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className="bg-black w-full h-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-          <img src="/images/logo.png" alt="logo" className="h-12" />
-
-          <div className="flex justify-center">
-            <div
-              className="bg-black bg-opacity-70 px-16 py-16 self-center 
-                                      mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full"
-            >
-              <h2 className="text-white text-4xl mb-8 font-semibold">
-                {variant == "login" ? "Giriş Yap" : "Kayıt Ol"}
-              </h2>
-
-              <div className="flex flex-col gap-4">
-                {variant == "register" && (
-                  <Input
-                    id="name"
-                    label="Kullanıcı adı"
-                    onChange={(e: any) => setName(e.target.value)}
-                    type="name"
-                    value={name}
-                  />
-                )}
-
-                <Input
-                  id="email"
-                  label="E-posta"
-                  onChange={(e: any) => setEmail(e.target.value)}
-                  type="email"
-                  value={email}
-                />
-                <Input
-                  id="Password"
-                  label="Şifrenizi girin"
-                  onChange={(e: any) => setPassword(e.target.value)}
-                  type="password"
-                  value={password}
-                />
-              </div>
-              <button
-                onClick={variant === "login" ? login : register}
-                className="bg-red-600 py-3 text-white font-medium rounded-md 
-                                 w-full mt-10 hover:bg-red-700 transation"
-              >
-                {variant == "login" ? "Giriş Yap" : "Kaydol"}
-              </button>
-              <p className="text-neutral-500 mt-6">
-                {variant == "login"
-                  ? "Netflix'e katılmak ister misin?"
-                  : "Netflix hesabın var mı?"}
-                <span
-                  onClick={toggleVariant}
-                  className="text-white font-medium ml-1 hover:underline cursor-pointer"
-                >
-                  {variant == "login" ? "Şimdi kaydolun" : "Giriş kapın"}
-                </span>
-                .
-              </p>
-            </div>
-          </div>
+          <img src="/images/logo.png" className="h-12" alt="Logo" />
         </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign in" : "Register"}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === "register" && (
+                <Input
+                  id="name"
+                  type="text"
+                  label="Username"
+                  value={name}
+                  onChange={(e: any) => setName(e.target.value)}
+                />
+              )}
+              <Input
+                id="email"
+                type="email"
+                label="Email address or phone number"
+                value={email}
+                onChange={(e: any) => setEmail(e.target.value)}
+              />
+              <Input
+                type="password"
+                id="password"
+                label="Password"
+                value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
+              />
+            </div>
+            <button
+              onClick={variant === "login" ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+            >
+              {variant === "login" ? "Login" : "Sign up"}
+            </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div
+                onClick={() => signIn("google", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FcGoogle size={32} />
+              </div>
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+              >
+                <FaGithub size={32} />
+              </div>
+            </div>
+            <p className="text-neutral-500 mt-12">
+              {variant === "login"
+                ? "First time using Netflix?"
+                : "Already have an account?"}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                {variant === "login" ? "Create an account" : "Login"}
+              </span>
+              .
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default auth;
+export default Auth;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,39 @@
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const sesion = await getSession(context);
+
+  if (!sesion) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 export default function Home() {
   return (
     <>
-      <h1 className="
-        text-blue-900
+      <h1
+        className="
+        text-blue-500
         font-bold
         text-5xl
-        ">Netflix Clone</h1>
+        "
+      >
+        Netflix Clone
+      </h1>
+
+      <button onClick={() => signOut()} className="bg-red-500 h-10 px-5 ">
+        Logout
+      </button>
     </>
-  )
+  );
 }
